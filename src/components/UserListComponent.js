c928195e6b61eba27b18b3df9742f18976c115f2
Emil <emilelizade932@gmail.com>
2022-04-14 15:05:29 +0400

    init

--- src/components/UserListComponent.js
+++ src/components/UserListComponent.js
@@ -1,90 +1,60 @@
 import React, { Component } from 'react'
 import { Table } from 'reactstrap'
 
-export default class  UserListComponent extends Component {
-  render() {
-    return (
-      <div className='container mt-5' > 
-      <button  className='btn btn-primary' >Add</button>
-      <Table
->
-  <thead>
-    <tr>
-      <th>
-        #
-      </th>
-      <th>
-        First Name
-      </th>
-      <th>
-        Last Name
-      </th>
-      <th>
-        Username
-      </th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">
-        1
-      </th>
-      <td>
-        Mark
-      </td>
-      <td>
-        Otto
-      </td>
-      <td>
-        @mdo
-      </td>
-      <td>
-          <button className='btn btn-warning' >Edit</button>
-          &nbsp;
-          <button className='btn btn-danger' >Delete</button>
-      </td>
-    </tr>
-    <tr>
-      <th scope="row">
-        2
-      </th>
-      <td>
-        Jacob
-      </td>
-      <td>
-        Thornton
-      </td>
-      <td>
-        @fat
-      </td>
-      <td>
-          <button className='btn btn-warning' >Edit</button>
-          &nbsp;
-          <button className='btn btn-danger' >Delete</button>
-      </td>
-    </tr>
-    <tr>
-      <th scope="row">
-        3
-      </th>
-      <td>
-        Larry
-      </td>
-      <td>
-        the Bird
-      </td>
-      <td>
-        @twitter
-      </td>
-      <td>
-          <button className='btn btn-warning' >Edit</button>
-          &nbsp;
-          <button className='btn btn-danger' >Delete</button>
-      </td>
-    </tr>
-  </tbody>
-</Table>
-      </div>
-    )
-  }
+export default class UserListComponent extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className='container mt-5' >
+                <button className='btn btn-primary' >Add</button>
+                <Table
+                >
+                    <thead>
+                        <tr>
+                            <th>
+                                #
+                            </th>
+                            <th>
+                                First Name
+                            </th>
+                            <th>
+                                Last Name
+                            </th>
+                            <th>
+                                Username
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.props.users.map((user) => (
+                                <tr>
+                                    <th scope="row"> {user.id}
+                                    </th>
+                                    <td>
+                                        {user.name}
+                                    </td>
+                                    <td>
+                                        {user.surname}
+                                    </td>
+                                    <td>
+                                        {user.username}
+                                    </td>
+                                    <td>
+                                        <button className='btn btn-warning' >Edit</button>
+                                        &nbsp;
+                                        <button className='btn btn-danger' >Delete</button>
+                                    </td>
+                                </tr>
+
+
+                            ))}
+
+                    </tbody>
+                </Table>
+            </div>
+        )
+    }
 }
--- src/pages/HomePages.js
+++ src/pages/HomePages.js
@@ -3,22 +3,48 @@ import { Navbar, NavbarBrand } from 'reactstrap'
 import UserListComponent from '../components/UserListComponent'
 
 export default class HomePages extends Component {
-  render() {
-    return (
-        <div>
-        <Navbar
-          color="light"
-          expand="md"
-          light
-        >
-          <NavbarBrand href="/">
-            react-intro
-          </NavbarBrand>
-          
-        </Navbar>
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: [
+                {
+                    id: "1",
+                    name: "Mark",
+                    surname: "Otto",
+                    username:  "mdo",
+                },
+                {
+                    id: "2",
+                    name: "Jacob",
+                    surname: "Thornton",
+                    username: "fat",
+                },
+                {
+                    id: "3",
+                    name: "Larry",
+                    surname:  "Bird",
+                    username: "twitter",
+                }
 
-        <UserListComponent />
-      </div>
-    )
-  }
+            ]
+        }
+    }
+    render() {
+        return (
+            <div>
+                <Navbar
+                    color="light"
+                    expand="md"
+                    light
+                >
+                    <NavbarBrand href="/">
+                        react-intro
+                    </NavbarBrand>
+
+                </Navbar>
+
+                <UserListComponent  users = {this.state.users}/>
+            </div>
+        )
+    }
 }
